afb0021460bbf3e90c7822474c001e6cca69b794
cbconsultingga <cb.consulting.ga@gmail.com>
2025-08-31 12:45:50 -0400

    tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,9 @@
-
 import type { Config } from 'tailwindcss'
 
 export default {
   content: ['./index.html', './src/**/*.{ts,tsx}'],
-  theme: { extend: {} },
+  theme: {
+    extend: {},
+  },
   plugins: [],
 } satisfies Config
